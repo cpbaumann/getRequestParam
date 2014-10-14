9be9191f3e8085f54d3daaed77fd2511b2a04433
cpbaumann <cpbaumann@googlemail.com>
2014-10-14 10:14:01 +0200

    spaces

--- getRequestParam.php
+++ getRequestParam.php
@@ -10,9 +10,12 @@
  * PROPERTIES:
  *
  * &name string required
- * &type string required Default: `GET`
- * &allowedValues array optional. 
+ * &type string required Default: 'GET'
+ * &allowedValues list (comma separated) optional.  Default: false
  * &allowedTypes string optional. (naturalNumbers 1,2,3,4,) 
+ * &default string optional. Default: '' Return if request is not set or empty.
+ * &fallbackvalue string optional. Default: '' Return if request value validation fails
+ *
  *
  * USAGE:
  *
@@ -20,17 +23,17 @@
  *
  *
  * [[!getRequestParam? 
- *	  &name=`fruits` 
- *	  &allowedValues=`apple,peaches,pear,apricots` 
- *	  &fallbackvalue=`fallbackvalue` 
- *	  &default=`defaultvalue`
+ *    &name=`fruits` 
+ *    &allowedValues=`apple,peaches,pear,apricots` 
+ *    &fallbackvalue=`fallbackvalue` 
+ *    &default=`defaultvalue`
  * ]]
  *
  * [[!getRequestParam? 
  *    &name=`number` 
- * 	  &allowedTypes=`naturalNumbers` 
- * 	  &fallbackvalue=`fallbackvalue` 
- * 	  &default=`defaultvalue` 
+ *    &allowedTypes=`naturalNumbers` 
+ *    &fallbackvalue=`fallbackvalue` 
+ *    &default=`defaultvalue` 
  * ]]
  *
  */
@@ -39,17 +42,12 @@ $name = $modx->getOption('name', $scriptProperties);
 $type = $modx->getOption('type', $scriptProperties,'GET');
 $allowedValues = $modx->getOption('allowedValues', $scriptProperties,false);
 $allowedTypes = $modx->getOption('allowedTypes', $scriptProperties); 
-
-// default return if request is not set or empty
 $default = $modx->getOption('default', $scriptProperties,'');
-
-// return if request value is filtered and eliminated
 $fallbackvalue = $modx->getOption('fallbackvalue', $scriptProperties,'');
 
-// For debugging:
 $modx->log(modX::LOG_LEVEL_DEBUG , '[getRequestParam] called on page ' 
-	. $modx->resource->id 
-	. ' with the following properties: '
+    . $modx->resource->id 
+    . ' with the following properties: '
     . print_r($scriptProperties, true));
 
 // Verify Inputs
@@ -60,49 +58,49 @@ if (!isset($scriptProperties['name'])) {
 
 
 if ('POST' === $type) {
-	if (isset($_POST[$name]) AND (!empty($_POST[$name]))) {
+    if (isset($_POST[$name]) AND (!empty($_POST[$name]))) {
 
-		if ( true == $allowedValues ) {
-			$allowedValues = split(',',$allowedValues);
-			if (!in_array($_POST[$name], $allowedValues)) {
-				return $fallbackvalue;
-			}
-		}
+        if (true == $allowedValues) {
+            $allowedValues = split(',',$allowedValues);
+            if (!in_array($_POST[$name], $allowedValues)) {
+                return $fallbackvalue;
+            }
+        }
 
-		if ( 'naturalNumbers' === $allowedTypes ) {
-			if(!preg_match("/^[1-9]{1}[0-9]{0,}$/", $_POST[$name])){
-				return $fallbackvalue;
-			}
-		}
+        if ('naturalNumbers' === $allowedTypes) {
+            if(!preg_match("/^[1-9]{1}[0-9]{0,}$/", $_POST[$name])){
+                return $fallbackvalue;
+            }
+        }
 
-		$value = filter_var($_POST[$name], FILTER_SANITIZE_STRING);
+        $value = filter_var($_POST[$name], FILTER_SANITIZE_STRING);
 
-	}else{
-		$value = $default;
-	}
+    } else {
+        $value = $default;
+    }
 }
 
-if ( 'GET' === $type ) {
-	if (isset($_GET[$name]) AND (!empty($_GET[$name]))) {
+if ('GET' === $type) {
+    if (isset($_GET[$name]) AND (!empty($_GET[$name]))) {
 
-		if (true == $allowedValues ) {
-			$allowedValues = split(',',$allowedValues);			
-			if (!in_array($_GET[$name], $allowedValues)) {
-				return $fallbackvalue;
-			}
-		}
+        if (true == $allowedValues) {
+            $allowedValues = split(',',$allowedValues);            
+            if (!in_array($_GET[$name], $allowedValues)) {
+                return $fallbackvalue;
+            }
+        }
 
-		if ('naturalNumbers' === $allowedTypes) {
-			if(!preg_match("/^[1-9]{1}[0-9]{0,}$/", $_GET[$name])){
-				return $fallbackvalue;
-			}
-		}
+        if ('naturalNumbers' === $allowedTypes) {
+            if(!preg_match("/^[1-9]{1}[0-9]{0,}$/", $_GET[$name])){
+                return $fallbackvalue;
+            }
+        }
 
-		$value = filter_var($_GET[$name], FILTER_SANITIZE_STRING);
+        $value = filter_var($_GET[$name], FILTER_SANITIZE_STRING);
 
-	}else{
-		$value = $default;
-	}
+    } else {
+        $value = $default;
+    }
 }
 
 $output = $value;
